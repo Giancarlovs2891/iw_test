f25e8e0db8f492c531b01b5be8bf38be989911f8
giancarlovs2891 <giancarlovs2891@gmail.com>
2017-04-02 10:39:52 -0500

    scaffold projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :projects
   resources :tasks
   root to: "welcome#index"
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402153542) do
+ActiveRecord::Schema.define(version: 20170402153813) do
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20170402153542) do
   end
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
