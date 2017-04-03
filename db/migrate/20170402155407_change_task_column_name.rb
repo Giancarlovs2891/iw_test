5e887981db6b3b67080abcf4890b62bfc9c0904e
giancarlovs2891 <giancarlovs2891@gmail.com>
2017-04-02 20:13:20 -0500

    remove migrations

--- db/migrate/20170402155407_change_task_column_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTaskColumnName < ActiveRecord::Migration
-  def change
-    change_column :tasks, :total_time, :integer
-  end
-end
--- db/migrate/20170403005500_change_taks_column_type.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTaksColumnType < ActiveRecord::Migration
-  def change
-    change_column :tasks, :total_time, :integer, :default => 0
-  end
-end
