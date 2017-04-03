30a2b8afe1c590705fc27d549e44b09d76beea35
giancarlovs2891 <giancarlovs2891@gmail.com>
2017-04-02 19:57:58 -0500

    change on total_time for deploy

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402194507) do
+ActiveRecord::Schema.define(version: 20170403005500) do
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
